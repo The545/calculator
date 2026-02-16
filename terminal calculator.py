7f3ca3c9d590dfde2d51b7497c7e731b6be605aa
Glitch404 <114830939+The545@users.noreply.github.com>
2026-02-16 15:07:44 +0000

    new file:   terminal calculator.py

---
+++ terminal calculator.py
@@ -0,0 +1,18 @@
+num1 = float(input("Give me the ferst number you damb a_s:"))
+gg = input("now what you whant to do ? whith it ? Enter: ")
+num2 = float(input("Now the sacund one: "))
+
+if gg == "+":
+print("Don: " , num1 + num2)
+
+elif gg == "-":
+print("Don: " , num1 - num2)
+
+elif gg == "/":
+print("Don: " , num1 / num2)
+
+elif gg == "*":
+print("Don: " , num1 * num2)
+
+else:
+print("Fuck off self destrakshon in 3....2.....1 cpu will don?!#$")
